474dd4eac519c9d4472f2999f8283bb1d5444081
Shivansh Kuchchal <shivanshbox@gmail.com>
2023-07-17 13:54:54 +0530

    chore: step2 complete

--- cmd/raytracing/main.go
+++ cmd/raytracing/main.go
@@ -7,28 +7,55 @@ import (
 )
 
 const (
-	imageWidth  = 1920
-	imageHeight = 1080
+	aspectRatio = 16.0 / 9.0
+	imageWidth  = 1024.0
+	imageHeight = imageWidth / aspectRatio
+
+	viewportHeight = 2.0
+	viewportWidth  = aspectRatio * viewportHeight
+	focalLength    = 1.0
+)
+
+var (
+	origin     = pkg.NewVector(0, 0, 0)
+	horizontal = pkg.NewVector(viewportWidth, 0, 0)
+	vertical   = pkg.NewVector(0, viewportHeight, 0)
 )
 
 func main() {
+	lowerLeftCorner := origin.
+		Minus(horizontal.Divide(2)).
+		Minus(vertical.Divide(2)).
+		Minus(pkg.NewVector(0, 0, focalLength))
+
 	fmt.Printf("P3\n")
-	fmt.Printf("%d %d\n", imageWidth, imageHeight)
+	fmt.Printf("%d %d\n", int(imageWidth), int(imageHeight))
 	fmt.Printf("255\n")
 
 	for j := imageHeight - 1; j >= 0; j-- {
 		// Progress tracker.
-		debugf("\rLines remaining: %d", j)
+		debugf("\rLines remaining: %d", int(j))
 
 		for i := 0; i < imageWidth; i++ {
-			colour := pkg.NewColour(float32(i)/(imageWidth-1),
-				float32(j)/(imageHeight-1), 0.25)
+			x := float64(i) / (imageWidth - 1)
+			y := float64(j) / (imageHeight - 1)
 
-			fmt.Println(colour.GetPPMRow())
+			rayDirection := lowerLeftCorner.
+				Plus(horizontal.Multiply(x)).
+				Plus(vertical.Multiply(y))
+
+			ray := pkg.NewRay(origin, rayDirection)
+			fmt.Println(blueGradientColour(ray).GetPPMRow())
 		}
 	}
 
 	debugf("\nDone.\n")
+}
+
+func blueGradientColour(r *pkg.Ray) *pkg.Colour {
+	unitDirection := r.Direction.Direction()
+	t := 0.5 * (unitDirection.Y + 1)
+	return pkg.NewColour(1, 1, 1).LerpTo(pkg.NewColour(0.5, 0.7, 1.0), t)
 }
 
 // debugf can be used to print debugging info.
--- pkg/colour.go
+++ pkg/colour.go
@@ -3,16 +3,26 @@ package pkg
 import "fmt"
 
 type Colour struct {
-	R float32
-	G float32
-	B float32
+	R float64
+	G float64
+	B float64
 }
 
-func NewColour(r, g, b float32) *Colour {
+func NewColour(r, g, b float64) *Colour {
 	return &Colour{R: r, G: g, B: b}
 }
 
 func (c *Colour) GetPPMRow() string {
 	return fmt.Sprintf("%d %d %d", int(255.999*c.R),
 		int(255.999*c.G), int(255.999*c.B))
+}
+
+func (c *Colour) LerpTo(end *Colour, factor float64) *Colour {
+	oneMinusFactor := 1 - factor
+
+	return &Colour{
+		oneMinusFactor*c.R + factor*end.R,
+		oneMinusFactor*c.G + factor*end.G,
+		oneMinusFactor*c.B + factor*end.B,
+	}
 }
