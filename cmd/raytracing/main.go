ef2fdefaf709ff9830ccd4014736e3c653e53eab
Shivansh Kuchchal <shivanshbox@gmail.com>
2023-07-19 15:48:43 +0530

    step9 complete

--- cmd/raytracing/main.go
+++ cmd/raytracing/main.go
@@ -13,7 +13,7 @@ import (
 const (
 	aspectRatio = 16.0 / 9.0
 
-	imageWidth  = 480
+	imageWidth  = 1920
 	imageHeight = imageWidth / aspectRatio
 
 	maxDiffusionDepth = 50
@@ -21,46 +21,25 @@ const (
 	samplesPerPixel = 100
 )
 
-var (
-	hittableGroup = hittable.NewHittableGroup([]hittable.Hittable{
-		&hittable.Sphere{
-			Center: pkg.NewVector(-1, 0, -1),
-			Radius: 0.5,
-			Mat: &material.Dielectric{
-				RefractiveIndex: 1.5,
-			},
+var hittables = []hittable.Hittable{
+	// Ground.
+	&hittable.Sphere{
+		Center: pkg.NewVector(0, -100000, 0),
+		Radius: 100000,
+		Mat: &material.Lambertian{
+			Attenuation: pkg.NewColour(0.5, 0.5, 5),
 		},
-		&hittable.Sphere{
-			Center: pkg.NewVector(0, 0, -1),
-			Radius: 0.5,
-			Mat: &material.Lambertian{
-				Attenuation: pkg.NewColour(0.1, 0.2, 0.5),
-			},
-		},
-		&hittable.Sphere{
-			Center: pkg.NewVector(1, 0, -1),
-			Radius: 0.5,
-			Mat: &material.Metal{
-				Attenuation: pkg.NewColour(0.6, 0.4, 0.2),
-				Fuzz:        0,
-			},
-		},
-		// Ground.
-		&hittable.Sphere{
-			Center: pkg.NewVector(0, -100000.5, -1),
-			Radius: 100000,
-			Mat: &material.Lambertian{
-				Attenuation: pkg.NewColour(0.8, 0.8, 0),
-			},
-		},
-	})
-)
+	},
+}
 
 func main() {
 	start := time.Now()
 	defer func() { debugf("\nDone. Time taken: %+v\n", time.Since(start)) }()
 
 	camera := createCamera()
+
+	populateWorld()
+	hittableGroup := hittable.NewHittableGroup(hittables)
 
 	fmt.Printf("P3\n")
 	fmt.Printf("%d %d\n", int(imageWidth), int(imageHeight))
@@ -91,12 +70,12 @@ func main() {
 }
 
 func createCamera() *pkg.Camera {
-	lookFrom, lookAt := pkg.NewVector(-2, 2, 1), pkg.NewVector(0, 0, -1)
+	lookFrom, lookAt := pkg.NewVector(13, 2, 3), pkg.NewVector(0, 0, 0)
 
 	return pkg.NewCamera(
 		lookFrom, lookAt, pkg.NewVector(0, 1, 0),
 		20.0, aspectRatio,
-		0.6, lookFrom.Minus(lookAt).Magnitude(),
+		0.1, 10,
 	)
 }
 
@@ -129,4 +108,69 @@ func rayColour(ray *pkg.Ray, hittable hittable.Hittable, depth int) *pkg.Colour 
 // debugf can be used to print debugging info.
 func debugf(format string, a ...any) {
 	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+func populateWorld() {
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMat := pkg.RandomFloat()
+			center := pkg.NewVector(
+				float64(a)+0.9*pkg.RandomFloat(),
+				0.2,
+				float64(b)*0.9*pkg.RandomFloat(),
+			)
+
+			if center.Minus(pkg.NewVector(4, 0.2, 0)).Magnitude() <= 0.9 {
+				continue
+			}
+
+			if chooseMat < 0.8 {
+				sphereMat := material.Lambertian{Attenuation: pkg.RandomColor()}
+				hittables = append(hittables, &hittable.Sphere{
+					Center: center,
+					Radius: 0.2,
+					Mat:    &sphereMat,
+				})
+			} else if chooseMat < 0.95 {
+				sphereMat := material.Metal{
+					Attenuation: pkg.RandomColor(),
+					Fuzz:        pkg.RandomFloatBetween(0, 0.5),
+				}
+
+				hittables = append(hittables, &hittable.Sphere{
+					Center: center,
+					Radius: 0.2,
+					Mat:    &sphereMat,
+				})
+			} else {
+				sphereMat := material.Dielectric{RefractiveIndex: 1.5}
+				hittables = append(hittables, &hittable.Sphere{
+					Center: center,
+					Radius: 0.2,
+					Mat:    &sphereMat,
+				})
+			}
+		}
+	}
+
+	sphereMat1 := material.Dielectric{RefractiveIndex: 1.5}
+	hittables = append(hittables, &hittable.Sphere{
+		Center: pkg.NewVector(0, 1, 0),
+		Radius: 1.0,
+		Mat:    &sphereMat1,
+	})
+
+	sphereMat2 := material.Lambertian{Attenuation: pkg.NewColour(0.4, 0.2, 0.1)}
+	hittables = append(hittables, &hittable.Sphere{
+		Center: pkg.NewVector(-4, 1, 0),
+		Radius: 1.0,
+		Mat:    &sphereMat2,
+	})
+
+	sphereMat3 := material.Metal{Attenuation: pkg.NewColour(0.7, 0.6, 0.5), Fuzz: 0}
+	hittables = append(hittables, &hittable.Sphere{
+		Center: pkg.NewVector(4, 1, 0),
+		Radius: 1.0,
+		Mat:    &sphereMat3,
+	})
 }
