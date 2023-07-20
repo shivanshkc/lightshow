5e1c07d5a10f6365ae1f2337bcc92d0565259454
Shivansh Kuchchal <shivanshbox@gmail.com>
2023-07-20 19:42:06 +0530

    chore: material implementations complete

---
+++ pkg/mats/matte.go
@@ -0,0 +1,26 @@
+package mats
+
+import (
+	"raytracing/pkg/utils"
+)
+
+// Matte implements the material interface as a matte or Lambertian material.
+type Matte struct {
+	albedo *utils.Colour
+}
+
+// NewMatte returns a new Matte material.
+func NewMatte(albedo *utils.Colour) *Matte {
+	return &Matte{albedo: albedo}
+}
+
+func (m *Matte) Scatter(ray *utils.Ray, hitInfo *RayHit) (*utils.Ray, *utils.Colour, bool) {
+	scatterDir := hitInfo.Normal.Add(utils.Random.UnitVec3())
+
+	// Catch degenerate scatter direction.
+	if scatterDir.IsNearZero() {
+		scatterDir = hitInfo.Normal
+	}
+
+	return utils.NewRay(hitInfo.Point, scatterDir), m.albedo, true
+}
