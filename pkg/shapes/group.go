dddb303881270e053687b22d2520b99e88447a16
Shivansh Kuchchal <shivanshbox@gmail.com>
2023-07-20 16:48:12 +0530

    shapes package complete

---
+++ pkg/shapes/group.go
@@ -0,0 +1,45 @@
+package shapes
+
+import (
+	"raytracing/pkg/mats"
+	"raytracing/pkg/utils"
+)
+
+// Group represents a group of shapes.
+// It implements the Shape interface itself so it can be treated as a single Shape.
+//
+// Its implementation of the Shape interface returns the closest point-of-hit out of
+// all the shapes for the given ray.
+type Group struct {
+	shapes []Shape
+}
+
+// NewGroup creates a new Group instance.
+func NewGroup(shapes ...Shape) *Group {
+	return &Group{shapes: shapes}
+}
+
+// Hit returns the closest point-of-hit out of all the shapes for the given ray.
+func (g *Group) Hit(ray *utils.Ray, minD, maxD float64) (*mats.RayHit, bool) {
+	// hitAnything will be true if at least a single shape is hit.
+	hitAnything := false
+	// This will keep track of the closest point-of-hit so far.
+	closestSoFar := maxD
+	// This will keep the RayHit record for the closest hit.
+	var closestRayHit *mats.RayHit
+
+	// Loop over all shapes to determine the closest hit.
+	for _, shape := range g.shapes {
+		// Notice the usage of "closestSoFar" here. It leads to lots of calculation savings.
+		info, isHit := shape.Hit(ray, minD, closestSoFar)
+		if !isHit {
+			continue
+		}
+
+		hitAnything = true
+		closestSoFar = info.Distance
+		closestRayHit = info
+	}
+
+	return closestRayHit, hitAnything
+}
