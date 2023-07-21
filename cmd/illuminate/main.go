ba065ad6e4674e5851aa6dec40fa77a060fbfaaf
Shivansh Kuchchal <shivanshbox@gmail.com>
2023-07-22 01:35:10 +0530

    ppm encoder added

--- cmd/illuminate/main.go
+++ cmd/illuminate/main.go
@@ -34,7 +34,7 @@ var renderOptions = &renderer.Options{
 	SkyColour:         utils.NewColour(0.5, 0.75, 1.0),
 	MaxDiffusionDepth: 50,
 	SamplesPerPixel:   25,
-	OutputFile:        "./dist/image.png",
+	OutputFile:        "./dist/image.ppm",
 	ProgressLogger:    func(s string) { debugf("\r%s.", s) },
 }
 
