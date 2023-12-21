588c5cab0d6c9814efcb31fd4a905ad99a867504
Shivansh Kuchchal <shivanshbox@gmail.com>
2023-12-21 19:55:13 +0530

    feat(core): add progress indicator

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 
 int main() {
-    int image_width = 256;
-    int image_height = 256;
+    // Image dimensions.
+    int image_width = 480;
+    int image_height = 270;
 
+    // PPM headers.
     printf("P3\n");
     printf("%d %d\n", image_width, image_height);
     printf("255\n");
 
     for (int j = 0; j < image_height; ++j) {
+        // Progress indicator.
+        fprintf(stderr, "\rLines remaining: %d", image_height - j - 1);
+
         for (int i = 0; i < image_width; ++i) {
             double r = (double) i / (image_width-1);
             double g = (double) j / (image_height-1);
@@ -21,4 +26,7 @@ int main() {
             printf("%d %d %d\n", ir, ig, ib);
         }
     }
+
+    // Newline after the progress indicator.
+    fprintf(stderr, "\n");
 }
