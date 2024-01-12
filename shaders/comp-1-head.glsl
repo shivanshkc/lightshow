bb640e0e8f734e7b201682c40da8aeb78906adf8
Shivansh Kuchchal <shivanshbox@gmail.com>
2024-01-12 15:46:42 +0530

    chore: add sphere movement

--- shaders/comp-1-head.glsl
+++ shaders/comp-1-head.glsl
@@ -1,8 +1,14 @@
 #version 460
+
 layout (local_size_x = 16, local_size_y = 16) in;
 
 // The texture to store the output.
 layout (rgba32f, binding = 0) uniform image2D imgOutput;
+
+// Positions of the bodies.
+layout (binding = 1) buffer Inputs {
+    float positions[];
+};
 
 // The starting seed taken as input.
 uniform float init_seed;
