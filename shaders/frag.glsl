2e5c9f63fcc8f6945734dbfd9808dbaa2ddde6a5
Shivansh Kuchchal <shivanshbox@gmail.com>
2024-01-14 14:12:32 +0530

    chore: more refactoring

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -4,8 +4,8 @@ in vec2 TexCoord;
 
 out vec4 FragColor;
 
-uniform sampler2D ourTexture;
+uniform sampler2D fullTexture;
 
 void main() {
-    FragColor = texture(ourTexture, TexCoord);
+    FragColor = texture(fullTexture, TexCoord);
 }
